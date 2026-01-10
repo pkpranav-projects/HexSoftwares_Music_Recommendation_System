506c6a8e4fac6fde61f68707c1966f77e8f7d664
pkpranav-projects <pk.pranav7685@gmail.com>
2026-01-10 22:15:40 +0530

    Create recommendation.py

---
+++ src/recommendation.py
@@ -0,0 +1,8 @@
+# Music Recommendation System
+# Hex Softwares Internship
+
+def recommend_songs(user_input):
+    return ["Song A", "Song B", "Song C"]
+
+if __name__ == "__main__":
+    print(recommend_songs("user"))
